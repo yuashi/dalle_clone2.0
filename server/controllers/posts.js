3755962a9f1ba3904e4a86310c6e8c73bfbe31ee
yuashi <singh9ayushi9@gmail.com>
2023-03-23 13:28:28 +0530

    comment added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,6 +4,7 @@ import Post from "../mongodb/models/post.js";
 
 dotenv.config();
 
+//cloudinary configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
